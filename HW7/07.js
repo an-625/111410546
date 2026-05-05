dd21248ff8de64a5422450fd216822971997b427
an-625 <anson09487@gmail.com>
2026-05-05 09:42:48 +0800

    Create 07.js

---
+++ HW7/07.js
@@ -0,0 +1,9 @@
+function fakeGet(sql, params, callback) {
+    const fakeRow = { id: 1, title: "掌握 JavaScript 函數", content: "這是一篇關於 Callback 的文章..." };
+    callback(null, fakeRow);
+}
+
+fakeGet("SELECT * FROM posts WHERE id = ?", [1], (err, row) => {
+    if (err) console.error("查詢失敗");
+    else console.log("抓到的文章標題是：", row.title);
+});
